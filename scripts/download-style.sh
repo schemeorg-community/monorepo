18dd1979ffd3a320b5a0399b067a338c32e8f42c
Lassi Kortela <lassi@lassi.io>
2023-02-21 16:34:37 +0200

    Use production stylesheet

--- scripts/download-style.sh
+++ scripts/download-style.sh
@@ -3,4 +3,4 @@ set -eu
 cd "$(dirname "$0")"
 cd ..
 curl --location --fail --silent --show-error -o shared/schemeorg.css \
-    https://www.staging.scheme.org/schemeorg.css
+    https://www.scheme.org/schemeorg.css
