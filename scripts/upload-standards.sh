e3000ea2e911b8f8b60127f404aa3e536afe3438
Arthur A. Gleckler <aag@alum.mit.edu>
2026-06-09 14:23:28 -0700

    Fix order of submodule update.

--- scripts/upload-standards.sh
+++ scripts/upload-standards.sh
@@ -2,5 +2,5 @@
 set -eu
 cd "$(dirname "$0")"
 cd ..
-rsync -crv shared/ standards/ tuonela.scheme.org:/production/standards/www/
 git submodule update --init standards/r7rs-html5
+rsync -crv shared/ standards/ tuonela.scheme.org:/production/standards/www/
